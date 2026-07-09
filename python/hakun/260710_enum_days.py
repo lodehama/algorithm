afe85c4898453b58af74f55ce553494cdaeec710
lodehama <lodenx24@gmail.com>
2026-07-10 08:08:10 +0900

    Create 260710_enum_days.py

---
+++ python/hakun/260710_enum_days.py
@@ -0,0 +1,15 @@
+days_name = [
+  "sunday",
+  "monday",
+  "tuesday",
+  "wednesday",
+  "thursday",
+  "friday",
+  "saturday"
+]
+
+SUN, MON, TUE, WED, THU, FRI, SAT = range(7)
+
+day = WED
+
+print(f"{day}번째 요일은 {days_name[day]}입니다.")
