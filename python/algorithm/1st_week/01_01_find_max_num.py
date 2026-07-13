d540a8519d6b5999af4e9eb42c3324a7278805e4
lodehama <lodenx24@gmail.com>
2026-07-13 15:42:23 +0900

    260713

--- python/algorithm/1st_week/01_01_find_max_num.py
+++ python/algorithm/1st_week/01_01_find_max_num.py
@@ -8,6 +8,30 @@ def find_max_num(array):
 			return number
 
 
+def find_max_num2(array):
+	max_number = array[0]
+
+	for number in array:
+		if number > max_number:
+			max_number = number
+	return max_number
+
+
+# 1. 하나의 원소를 다른 원소들과 비교해서 최대값을 찾기
+# 3, 5, 6, 1, 2, 4
+#
+# 3 -> 5, 6, 1, 2, 4
+# 5 -> 6, 1, 2, 4
+# 6 -> 1, 2, 4
+# 1 -> 2, 4
+# 2 -> 4
+
+# 2. 하나의 변수를 잡아서 그 변수와 비교하며 최대값을 찾기
+# 3, 5, 6, 1, 2, 4
+#
+# max_num = 6
+
+
 print("정답 = 6 / 현재 풀이 값 = ", find_max_num([3, 5, 6, 1, 2, 4]))
 print("정답 = 6 / 현재 풀이 값 = ", find_max_num([6, 6, 6]))
 print("정답 = 1888 / 현재 풀이 값 = ", find_max_num([6, 9, 2, 7, 1888]))
